a1146de82594ff0b305aec80de18f0882894a3a9
amanrk2801 <amankumbhalwar09@gmail.com>
2024-05-02 17:38:09 +0530

    array

--- src/Array/ArrayDemo.java
+++ src/Array/ArrayDemo.java
@@ -7,7 +7,7 @@ public class ArrayDemo {
     public static void main(String[] args) {
 
         //create a array
-        int arr[]=new int[500];
+        int arr[] = new int[500];
         /*int []arr1 = new int[5];
         int[] arr3 = new int[5];
         int arr4[]={1,2,3,4,5};*/
@@ -23,21 +23,33 @@ public class ArrayDemo {
         System.out.println(arr[2]);*/
 
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("************Storing value***************");
-        for(int i=0;i<5;i++)
-        {
-            System.out.println("Enter the value for index = "+i);
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter the value for index = " + i);
+            arr[i] = sc.nextInt();
         }
+
+        int baseAddress = System.identityHashCode(arr); // Getting base address using identityHashCode()
+        int sizeOfInt = Integer.SIZE / 8; // Size of int in bytes
 
         System.out.println("************Fetching value***************");
-        for(int i=0;i<5;i++)
-        {
-            System.out.println("value for index "+i+" = "+arr[i]);
-
+        for (int i = 0; i < 5; i++) {
+            int address = baseAddress + (sizeOfInt * i);
+            System.out.println("value for index " + i + " = " + arr[i]);
+            System.out.println("Address of arr[" + i + "] = " + address);
         }
-
         System.out.println(arr);
     }
 }
+
+// Addressing of array
+// (i.e. assume 100 is the base address otherwise in real [I@4edde6e5)
+// [I@4edde6e5 -> hashcode encoded address
+// arr[0] -> 100 + (4*0) => 100
+// arr[1] -> 100 + (4*1) => 104
+// arr[2] -> 100 + (4*2) => 108
+// arr[3] -> 100 + (4*3) => 112
+// arr[4] -> 100 + (4*4) => 116
+
+
--- src/Basic/Monkey.java
+++ src/Basic/Monkey.java
@@ -7,7 +7,7 @@ public class Monkey
         System.out.println("Hello, World!");
         System.out.println("I'm in main method");
         // Monkey m = new Monkey(); // creating a object
-        // m.m1(); // when m1 and m2 are non-static method and main method is static
+        // m.m1(); // when m1 and m2 are non-static method and main method is static then first create object with the help of class name and then call it
         // m1(); // when we make m1 and m2 static and main method is also static we directly call method without creating object m.
         Monkey.m1(); // when main method and other methods are static we can directly call with class_name.method
     }
